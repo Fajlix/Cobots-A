bd22e946b96028ea8ab780abae9e80489afe769a
fajlix <felix@oliv.one>
2022-03-26 01:12:13 +0100

    added both perception and planning packages, still getting errors for planning

--- tools/create_container.sh
+++ tools/create_container.sh
@@ -3,7 +3,7 @@ sudo docker rm -f cobotsA
 sudo docker run -i -d --gpus all --name cobotsA --network host \
         --privileged -v /dev:/dev -e DISPLAY=$DISPLAY -e QT_X11_NO_MITSHM=1 \
         -v /tmp/.X11-unix:/tmp/.X11-unix \
-        -v $HOME/docker_Cobots-A:/root/ocrtoc_ws/src \
+        -v $HOME/Cobots-A:/root/ocrtoc_ws/src \
         -v $HOME/Desktop/docker:/root/upload \
         registry.cn-hangzhou.aliyuncs.com/ocrtoc2021/release:2.1
 sudo xhost +local:`docker inspect --format='{{ .Config.Hostname }}' cobotsA`
